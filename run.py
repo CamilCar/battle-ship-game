d27a870e48fccf6c9b36438f98849a064da97ce6
CamilCar <cc.carsbo@gmail.com>
2023-01-24 18:54:37 +0000

    Initial commit. Add player game structure

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Game:
+    """
+    Game class. Keeps track of game scores, 
+    game progress, ships.
+    """
+    def __init__(self,player_name):
+        self.player_name = player_name
+
+def ask_player_name():
+    print("Welcome to Battle Ship")
+    player_name = input("Enter player name \n")
+    print(f"Welcome {player_name}, your game is starting...")
+    
+    game = Game(player_name)
+    print(game.player_name)
+
+
+ask_player_name()
