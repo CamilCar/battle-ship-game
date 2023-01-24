3de62daa013d58e83932eab0eec5b652b16c3d78
CamilCar <cc.carsbo@gmail.com>
2023-01-24 19:43:22 +0000

    Create two boards

--- run.py
+++ run.py
@@ -5,14 +5,34 @@ class Game:
     """
     def __init__(self,player_name):
         self.player_name = player_name
+        
+    def create_boards(self, size):
+        board = [["." for x in range(size)] for y in range(size)]
+        self.player_board = board
+        self.computer_board = board
+
+    def display_boards(self):
+        print("Computer's Board:")
+        for row in self.computer_board:
+            print(" ".join(row))
+
+        print(f"{self.player_name}'s Board:")
+        for row in self.player_board:
+            print(" ".join(row))
+
 
 def ask_player_name():
     print("Welcome to Battle Ship")
     player_name = input("Enter player name \n")
     print(f"Welcome {player_name}, your game is starting...")
     
+    return player_name
+    
+def main():
+    player_name = ask_player_name()
     game = Game(player_name)
-    print(game.player_name)
+    size = 5
+    game.create_boards(size)
+    game.display_boards()
 
-
-ask_player_name()
+main()
